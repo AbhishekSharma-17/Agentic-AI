24c3be36ad70e4e5a93ef8b60e903a59f00b16cf
Abhishek Personal <2001abhishek17@gmail.com>
2025-04-13 23:04:18 +0530

    vgvg

--- Agno/basic.py
+++ Agno/basic.py
@@ -1,6 +1,10 @@
 from agno.agent.agent import Agent
 from agno.models.openai import OpenAIChat
+from dotenv import load_dotenv
+import os
 
-agent = Agent(model=OpenAIChat(),description="YOu are a unique joke writer who writes nice rhyming jokes",markdown=True)
+load_dotenv()
+
+agent = Agent(model=OpenAIChat(api_key = os.getenv("OPENAI_API_KEY")),description="YOu are a unique joke writer who writes nice rhyming jokes",markdown=True)
 
 agent.print_response("Tell me a joke about cats.",stream=True)
