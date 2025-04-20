31ee19053b388289afce198579afd945a700c384
Abhishek Personal <2001abhishek17@gmail.com>
2025-04-21 03:04:17 +0530

    feat: add reasoning agent with OpenAI and YFinance tools
    Introduce a new reasoning agent that utilizes OpenAI for chat capabilities and YFinance for stock-related data. The agent is configured to display data in tables, include sources, and generate concise reports

---
+++ Agno/agents/reasoning_agent.py
@@ -0,0 +1,40 @@
+from agno.agent import Agent
+from agno.models.aws import AwsBedrock
+from agno.models.openai import OpenAIChat
+from agno.tools.reasoning import ReasoningTools
+from agno.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+agent = Agent(
+    model=AwsBedrock(
+                    id="us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+                    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                    aws_region="us-east-1",
+                     ),
+    # model=OpenAIChat(id="o4-mini-2025-04-16"),
+    tools=[
+        ReasoningTools(add_instructions=True),
+        YFinanceTools(
+            stock_price=True,
+            analyst_recommendations=True,
+            company_info=True,
+            company_news=True,
+        ),
+    ],
+    instructions=[
+        "Use tables to display data.",
+        "Include sources in your response.",
+        "Only include the report in your response. No other text.",
+    ],
+    markdown=True,
+)
+agent.print_response(
+    "Write a report on Kroolo",
+    stream=True,
+    show_full_reasoning=True,
+    stream_intermediate_steps=True,
+)
